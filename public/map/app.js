df9a51017f7a5d9bc5f9eba7e959a0e2de03a199
Andrew McGill <arm5077@gmail.com>
2015-03-18 13:18:43 -0400

    Added basic pinlaying

--- public/map/app.js
+++ public/map/app.js
@@ -10,6 +10,21 @@ app.config(['$locationProvider', function($locationProvider) {
 
 app.controller("travelController", ["$scope", "$http", "$location", function($scope, $http, $location){
 	
+	$scope.colors = [
+		"#145786",
+		"#952226",
+		"#B5A99B",
+		"#FAA61A",
+		"#2B7064"
+	];
+	
+	// Initialize map
+	var map = L.map('map').setView([39.8282, -98.5795], 4);
+	
+	// Change this tilelayer
+	L.tileLayer('http://{s}.tiles.mapbox.com/v3/arm5077.78b64076/{z}/{x}/{y}.png', {}).addTo(map);
+	
+	
 	
 	
 	$scope.parameters = $location.search();
@@ -24,8 +39,46 @@ app.controller("travelController", ["$scope", "$http", "$location", function($sc
 		url: "/trips",
 		method: "GET",
 		params: $scope.parameters
-	}).success(function(data,status,headers,config){
+	}).success(function(data, status, headers, config){
 		
+		// Build new array with candidate as the base
+		$scope.candidates = {};
+		data.results.forEach(function(trip){
+			if( !$scope.candidates.hasOwnProperty( trip.candidate ) )
+				$scope.candidates[trip.candidate] = [];
+			$scope.candidates[trip.candidate].push(trip);
+		});
+		
+		// Loop through candidates
+		currentColor = 0
+		for( name in $scope.candidates ){
+			if( $scope.candidates.hasOwnProperty(name) ){
+			
+				if( currentColor < ($scope.colors.length -1) )
+					currentColor++;
+				else
+					currentColor = 0; 
+		
+				// Cycle through trips
+				$scope.candidates[name].forEach( function(trip){
+					// Loop through stops
+					for( i=0; i<=trip.stops.length - 1; i++) {
+						
+						// Add marker
+						L.circleMarker([trip.stops[i].lat, trip.stops[i].lng], {
+							fillColor: $scope.colors[currentColor],
+							fillOpacity: .8,
+							weight: 5,
+							color: $scope.colors[currentColor],
+							strokeOpacity:.4,
+							radius: 10
+						} ).addTo(map);
+					}
+				});
+			}	
+		}
+	}).error(function(data, status, headers, config){
+		throw "Looks like we couldn't get data: " + data;
 	});
 	
 }])
