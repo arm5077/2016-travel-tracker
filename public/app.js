18ed4313a8753de64a62515942f0394dde508d48
Andrew McGill <arm5077@gmail.com>
2015-03-24 22:04:22 -0400

    Adds frequently vistied states

--- public/app.js
+++ public/app.js
@@ -10,6 +10,9 @@ app.config(['$locationProvider', function($locationProvider) {
 
 app.controller("travelController", ["$scope", "$http", "$location", function($scope, $http, $location){
 	$scope.parameters = $location.search();
+	$scope.padding = .1;
+	$scope.minHeight = 10;
+	$scope.chartHeight = 60;
 	
 	$http({
 		url: "/candidates",
@@ -32,4 +35,44 @@ app.controller("travelController", ["$scope", "$http", "$location", function($sc
 		return name.replace(" ", "") + ".png";
 	}
 	
+	$scope.barWidth = function(data) {
+		return (100 / 5) * (1 - $scope.padding);
+		//return (100 / data.length) - ($scope.padding / (data.length - 1));
+	};
+	
+	$scope.barHeight = function(value, data){
+		height = value / Math.max.apply(null, data.map(function(d){ return d.count })) * $scope.chartHeight;
+		if( height < $scope.minHeight )
+			return $scope.minHeight;
+		else 
+			return height;
+
+	};
+	
+	$scope.barTop = function(value, data){
+		if( $scope.barHeight(value, data) < $scope.minHeight )
+			return $scope.chartHeight - $scope.minHeight;
+		else 
+			return $scope.chartHeight - $scope.barHeight(value, data);
+	};	
+	
+	$scope.barLeft = function(value, data) {
+		index = data.map(function(data){ return data.state}).indexOf(value);
+		return index * $scope.barWidth(data) + (index * $scope.padding * (100 / (4 ) ) );
+	};
+
+	
 }]);
+
+app.directive("bar", function() {
+	return {
+		restrict: 'E',
+		link: function(scope, element, attr) {
+			element.css({
+				width: scope.barWidth(scope.candidate["most-visited"]) + "%"
+			});
+		}
+	};	
+
+});
+
