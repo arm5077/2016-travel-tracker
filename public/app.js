74da5e44d639757155171649c8bbdb4333edecdf
Andrew McGill <arm5077@gmail.com>
2015-03-25 04:11:27 -0400

    Changes to local time hopefully

--- public/app.js
+++ public/app.js
@@ -50,7 +50,7 @@ app.controller("travelController", ["$scope", "$http", "$location", "$sce", func
 	
 	
 	$scope.imageURL = function(name){
-		return name.replace(" ", "") + ".png";
+		return name.replace(" ", "").toLowerCase() + ".png";
 	}
 	
 	$scope.barWidth = function(data) {
