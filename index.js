7df7b69eb3b00cf2c637294dba2f24acedc02354
Andrew McGill <arm5077@gmail.com>
2015-11-16 01:25:36 -0500

    Add CORS

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.listen(port, function(){
 	console.log("We're live at port " + port + ".");
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain);
+
+
 // Set up static page (main page)
 app.use("/", express.static(__dirname + "/public/"));
 
